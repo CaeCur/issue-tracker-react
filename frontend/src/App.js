b1e38aa94d078805e996aa36a56386cf3090e4b4
Caelan <caelan.curry@gmail.com>
2022-06-30 15:53:05 +0200

    Added routing with prelim page formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,29 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+//pages and components
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Issues from "./pages/Issues";
+import CreateIssue from "./pages/CreateIssue";
+
+//mui
+import { Container } from "@mui/material";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/issues" element={<Issues />} />
+            <Route path="/create-issue" element={<CreateIssue />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
